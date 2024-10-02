46fcce551f0d8b88917808e4fc5d573e1288eff6
ilestegor <99200163+ilestegor@users.noreply.github.com>
2024-10-02 15:17:36 +0300

    added test job for CI

---
+++ lab1/test/mod.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author ilestegor
+%%% @copyright (C) 2024, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 30. Sep 2024 7:49 PM
+%%%-------------------------------------------------------------------
+-module(mod).
+-author("ilestegor").
+-include_lib("eunit/include/eunit.hrl").
+
+simples_test() -> ?assert(1 =:= 1).
